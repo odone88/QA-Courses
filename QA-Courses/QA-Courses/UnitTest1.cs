790ca3316234ba6dca7f93e3ee098ce830c082ba
odone88 <mate.rutkowski@gmail.com>
2017-09-03 15:27:02 +0200

    Lesson 1 - Task 3
    Loggin script in c sharp

--- QA-Courses/QA-Courses/UnitTest1.cs
+++ QA-Courses/QA-Courses/UnitTest1.cs
@@ -22,7 +22,10 @@ namespace QA_Courses
         [Test]
         public void FirstTest()
         {
-            driver.Url = "http://www.google.com/";
+            driver.Url = "http://localhost/litecard/public_html/admin/login.php?redirect_url=%2Flitecard%2Fpublic_html%2Fadmin%2F";
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            driver.FindElement(By.Name("login")).Click();
         }
 
         [TearDown]
